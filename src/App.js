037eae7872c8b74576221a3f9220a75ca3ba228c
unknown <tonycarpenter@gmail.com>
2022-05-26 18:25:44 -0400

    Complete static comp and map data to table

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
-import fetchedStarshipData from './apiCalls';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState('')
+
+  useEffect(() => {
+    fetch("https://swapi.dev/api/starships")
+    .then(response => response.json())
+    .then(data => setData(data.results))
+    .catch(err => console.error(err))
+  }, [])
+
+  console.log(data)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* {fetchedStarshipData()} */}
-      </header>
+        <h1>Please Pick a Manufacturer:</h1>
+        <select>
+          <option key='All' value='All'>Show All Starships</option>  
+          {data.length === 0 ? <option>no data</option> : data.map( item => <option key={item.manufacturer} value={item.manufacturer}>{item.manufacturer}</option>)}
+        </select>
+        <table>
+          <tr className="table-header-row">
+            <th className="table-header top-left">Starship Name:</th>
+            <th className="table-header">Manufacturer:</th>
+            <th className="table-header">Cost (Credits):</th>
+            <th className="table-header">Hyperdrive Rating:</th>
+            <th className="table-header top-right">Passengers:</th>
+          </tr>
+          {data.length === 0 ? <td>no data</td> : data.map( item => {
+            return(
+              <tr>
+                <td className="table-row">{item.name}</td>
+                <td className="table-row">{item.manufacturer}</td>
+                <td className="table-row">{item.cost_in_credits}</td>
+                <td className="table-row">{item.hyperdrive_rating}</td>
+                <td className="table-row">{item.passengers}</td>
+              </tr>
+            )
+          })}
+        </table>
     </div>
   );
 }
 
-// create a simple web app containing a `<select>` input to allow a user to select a Starship `manufacturer`. 
 // In a `<table>` display a list of all Starships with the selected `manufacturer`. 
-// If no `manufacturer` is selected, display all starships in the table.
+
+
+// from me
+// style star wars like somehow? flying stars?
+// logic for sort?
 
 export default App;
