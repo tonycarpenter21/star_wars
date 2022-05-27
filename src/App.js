f9ba84d0846f3458b7c957480f4f1cbf372a48d2
unknown <tonycarpenter@gmail.com>
2022-05-26 20:09:21 -0400

    Refactor code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [data, setData] = useState('')
+  const [data, setData] = useState([])
   const [manufacturer, setManufacturer] = useState('All')
 
   useEffect(() => {
@@ -13,33 +13,26 @@ function App() {
   }, [])
 
   const filteredShips = (data, selectedManufacturer) => {
-    if (manufacturer === "All") {
-      return data
-    } else {
-      const filteredData = data.filter(item => item.manufacturer.includes(selectedManufacturer))
-      return filteredData
-    }
+    return manufacturer === "All" ? data : data.filter(item => item.manufacturer.includes(selectedManufacturer))
   }
 
   console.log(filteredShips(data, manufacturer))
+
+  const tableHeaders = ["Starship Name", "Manufacturer", "Cost (Credits)", "Hyperdrive Rating", "Passengers"]
 
   return (
     <div className="App">
         <h1>Please Pick a Manufacturer:</h1>
         <select onChange={(event) => setManufacturer(event.target.value)} value={manufacturer}>
-          <option key='All' value='All'>Show All Starships</option>  
-          {data.length === 0 ? <option>no data</option> : data.map( (item, index) => <option key={index} value={item.manufacturer}>{item.manufacturer}</option>)}
+          <option key="All" value="All">Show All Starships</option>  
+          {data.length === 0 ? <option>LOADING...</option> : data.map( (option, index) => <option key={index} value={option.manufacturer}>{option.manufacturer}</option>)}
         </select>
         <table>
           <tbody>
             <tr className="table-header-row">
-              <th className="table-header top-left">Starship Name:</th>
-              <th className="table-header">Manufacturer:</th>
-              <th className="table-header">Cost (Credits):</th>
-              <th className="table-header">Hyperdrive Rating:</th>
-              <th className="table-header top-right">Passengers:</th>
+              {tableHeaders.map((header, index) => <th key={index} className="table-header">{header}</th> )}
             </tr>
-            {filteredShips(data, manufacturer).length === 0 ? <tr><td>no data</td></tr> : filteredShips(data, manufacturer).map( (item, index) => {
+            {filteredShips(data, manufacturer).length === 0 ? <tr><td>LOADING...</td></tr> : filteredShips(data, manufacturer).map( (item, index) => {
               return(
                 <tr key={index}>
                   <td className="table-row">{item.name}</td>
